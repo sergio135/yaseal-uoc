d3e6e6ba296a46d7b5f99e73ebaec6f3fb0e23b2
Yanniel Fandiño García <33382521+YannFandino@users.noreply.github.com>
2018-11-15 22:03:13 +0100

    Rutas y Admin Panel
    Se añade el enrutamiento a las páginas de inicio de sesión y registro del panel de administración.

--- src/routes/home.php
+++ src/routes/home.php
@@ -5,6 +5,8 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
+require __DIR__ . '/../db/Classes/Controllers/AdminPanelController.php';
+
 // metodo que maneja cada una de las llamadas a la ruta inicial
 $app->get('/', function (Request $request, Response $response, array $args) {
     // $request: objeto que trae informacion sobre la peticion a la ruta.
@@ -12,10 +14,19 @@ $app->get('/', function (Request $request, Response $response, array $args) {
     // $args: deferentes argumentos pasados en la peticion.
 
 
-    // renderizamos la plantilla home.phtml 
+    // renderizamos la plantilla panel.phtml
     $response = $this->view->render($response, 'home.phtml', []);
 
     // devolver siempre el objeto $response
     return $response;
 });
+
+$app->get('/admin_panel', function (Request $request, Response $response, array $args) {
+    $response = $this->view->render($response, 'admin_panel/home.phtml', []);
+    return $response;
+});
+
+// Login y Registro
+$app->post('/admin_panel/login', AdminPanelController::class . ':login');
+$app->post('/admin_panel/register', AdminPanelController::class . ':register');
 ?>
