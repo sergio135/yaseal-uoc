e3dd02930e3c9875a0d158e3636480ee5da3cf73
Yanniel Fandiño García <33382521+YannFandino@users.noreply.github.com>
2018-11-15 21:59:59 +0100

    Fixes
    Se corrigen algunos literales y se añaden inserts para los roles

--- src/db/sql/db.sql
+++ src/db/sql/db.sql
@@ -1,8 +1,8 @@
 -- Crear esquema de la base de datos
-CREATE SCHEMA IF NOT EXISTS app_cms;
+CREATE SCHEMA IF NOT EXISTS p7_news_app;
 
 -- Seleccionar base de datos para su uso
-USE app_cms;
+USE p7_news_app;
 
 -- Crear tablas de la base de datos
 CREATE TABLE IF NOT EXISTS table_category (
@@ -10,40 +10,40 @@ CREATE TABLE IF NOT EXISTS table_category (
 	name VARCHAR(25) NOT NULL,
 	parent INT,
 	CONSTRAINT pk_category PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS table_role (
 	id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(25) NOT NULL,
 	description VARCHAR(100) NOT NULL,
 	CONSTRAINT pk_roles PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS table_permission (
 	id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(25) NOT NULL,
 	description VARCHAR(100) NOT NULL,
 	CONSTRAINT pk_permission PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS table_role_permission (
-	rol_id INT NOT NULL,
+	role_id INT NOT NULL,
 	permission_id INT NOT NULL,
-	CONSTRAINT pk_roles PRIMARY KEY (rol_id,permission_id),
-	CONSTRAINT fk_rp_role FOREIGN KEY (rol_id) REFERENCES table_role(id),
+	CONSTRAINT pk_roles PRIMARY KEY (role_id,permission_id),
+	CONSTRAINT fk_rp_role FOREIGN KEY (role_id) REFERENCES table_role(id),
 	CONSTRAINT fk_rp_permission FOREIGN KEY (permission_id) REFERENCES table_permission(id)
-) ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS table_user (
 	id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(50) NOT NULL,
 	email VARCHAR(100) NOT NULL,
-	password VARCHAR(20) NOT NULL,
+	password VARCHAR(255) NOT NULL,
 	date_registered DATE NOT NULL,
-	rol_id INT NOT NULL,
+	role_id INT NOT NULL,
 	CONSTRAINT pk_user PRIMARY KEY (id),
-	CONSTRAINT fk_user_roles FOREIGN KEY (rol_id) REFERENCES table_role(id)
-) ENGINE=InnoDB;
+	CONSTRAINT fk_user_roles FOREIGN KEY (role_id) REFERENCES table_role(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS table_news (
 	id INT NOT NULL AUTO_INCREMENT,
@@ -58,10 +58,8 @@ CREATE TABLE IF NOT EXISTS table_news (
 	editor INT,
 	category_id INT NOT NULL,
 	CONSTRAINT pk_news PRIMARY KEY (id),
-	CONSTRAINT fk_news_autor FOREIGN KEY (autor) REFERENCES table_user(id),
-	CONSTRAINT fk_news_editor FOREIGN KEY (editor) REFERENCES table_user(id),
 	CONSTRAINT fk_news_category FOREIGN KEY (category_id) REFERENCES table_category(id)
-) ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS table_keyword (
 	id INT NOT NULL AUTO_INCREMENT,
@@ -69,4 +67,9 @@ CREATE TABLE IF NOT EXISTS table_keyword (
 	name VARCHAR(100) NOT NULL,
 	CONSTRAINT pk_keyword PRIMARY KEY (id, news_id),
 	CONSTRAINT fk_keyword_news FOREIGN KEY (news_id) REFERENCES table_news(id)
-) ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO table_role (id, name, description) VALUES
+(1, 'admin', 'Administrador'),
+(2, 'editor', 'Editor'),
+(3, 'autor', 'Autor');
