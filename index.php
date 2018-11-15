704b26ff08d10e797c5d0619f16b540f8e3bcc5a
Yanniel Fandiño García <33382521+YannFandino@users.noreply.github.com>
2018-11-15 22:01:25 +0100

    Config DB
    Se añade configuración de base de datos a la app y un render para utilizar en los controladores

--- index.php
+++ index.php
@@ -4,7 +4,18 @@
 ///////////////////////////////////////////
 
 require './vendor/autoload.php';
-$app = new \Slim\App;
+
+session_start();
+
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+$config['db']['host']   = 'localhost';
+$config['db']['user']   = 'root';
+$config['db']['pass']   = '';
+$config['db']['dbname'] = 'p7_news_app';
+
+$app = new \Slim\App(['settings' => $config]);
 
 // Se crea un container que servira para añadir mas 
 // dependencias al proyecto y enlazarlas con el framework.
@@ -12,6 +23,19 @@ $container = $app->getContainer();
 
 // Añadios nueva depenencias para renderizar plantillas .phtml
 $container['view'] = new \Slim\Views\PhpRenderer('./src/views/');
+
+$container['db'] = function ($c) {
+    $db = $c['settings']['db'];
+    $pdo = new PDO('mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'],
+        $db['user'], $db['pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
+
+$container['renderer'] = function ($c) {
+    return new Slim\Views\PhpRenderer(__DIR__ . '/src/views');
+};
 
 // Importamos el resto de codigo de nuestra aplicacion 
 // categorizado por funcionalidad.
