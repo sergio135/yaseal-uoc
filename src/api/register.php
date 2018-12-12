75709eeb53df963bc88317f11dfd2d13816d2aa4
Yanniel Fandiño García <33382521+YannFandino@users.noreply.github.com>
2018-12-12 22:57:36 +0100

    Registro

--- src/api/register.php
+++ src/api/register.php
@@ -11,34 +11,34 @@ $app->post('/api/register', function(Request $req, Response $res, array $args) {
    
 
 
-    // $adminPanel = new AdminPanelController($this);
-    // $result = $adminPanel->register($req, $res, $args);
-    // if (array_key_exists("notification", $result)) {
-    //     $this->view->render($res, '/admin_panel/home.phtml', $result);
-    //     return;
-    // }
-    // $_SESSION['user'] = $result;
-    // return $res->withRedirect('panel', 301);
+     $adminPanel = new AdminPanelController($this);
+     $result = $adminPanel->register($req, $res, $args);
+     if (array_key_exists("notification", $result)) {
+         $this->view->render($res, '/admin_panel/auth.phtml', $result);
+         return;
+     }
+     $_SESSION['user'] = $result;
+     return $res->withRedirect('/yaseal-local/admin_panel/panel', 301);
 
     ///////////////////////////////////////////////// 
     // Aqui solo hay que devolver una respuesta afirmativa o un error, y ya se controla la redireccion desde el frontEnd con JS
     ///////////////////////////////////////////////// 
 
-    $statusCode = 200;
-    // la propiedad Error solo viene si hay algun error, si no, se envian los datos en la propiedad data
-    $data = [
-        'error' => [
-            'messagge' => 'El usuario no existe'
-        ],
-        'data' => [
-            'id' => 'a6s7d8a6sd',
-            'name' => 'Bob Jason',
-            'email' => 'asdasd@gmail.com'
-        ]
-    ];
-
-    // devolver siempre el objeto $response
-    return $res->withJson($data, $statusCode);
+//    $statusCode = 200;
+//    // la propiedad Error solo viene si hay algun error, si no, se envian los datos en la propiedad data
+//    $data = [
+//        'error' => [
+//            'messagge' => 'El usuario no existe'
+//        ],
+//        'data' => [
+//            'id' => 'a6s7d8a6sd',
+//            'name' => 'Bob Jason',
+//            'email' => 'asdasd@gmail.com'
+//        ]
+//    ];
+//
+//    // devolver siempre el objeto $response
+//    return $res->withJson($data, $statusCode);
 });
 
 ?>
