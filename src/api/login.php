5f25d301a6be7ff7c3792bae73622ef8aee2cc0c
Sergio Garcia <sergio.garcia@interacso.com>
2018-12-13 02:55:45 +0100

    implementar login y registro por ajax

--- src/api/login.php
+++ src/api/login.php
@@ -9,38 +9,17 @@ use \Psr\Http\Message\ResponseInterface as Response;
 $app->post('/api/login', function (Request $req, Response $res, array $args) {
 
     // los datos que vienen del formulario
-    $dataForm = json_decode($req->getBody());
-
-
+    $dataForm = json_decode($req->getBody(), true);
+    
     $adminPanel = new AdminPanelController($this);
-    $result = $adminPanel->login($req, $res, $args);
+    $result = $adminPanel->login($dataForm);
+    
+    
     if (array_key_exists("notification", $result)) {
-        $this->view->render($res, '/admin_panel/auth.phtml', $result);
-        return;
+        return $res->withJson(['error' => $result['notification']], 500);  
     }
     $_SESSION['user'] = $result;
-    return $res->withRedirect('/yaseal-local/admin_panel/panel', 301);
-
-
-    /////////////////////////////////////////////////
-    // Aqui solo hay que devolver una respuesta afirmativa o un error, y ya se controla la redireccion desde el frontEnd con JS
-    /////////////////////////////////////////////////
-
-    $statusCode = 200;
-    // la propiedad Error solo viene si hay algun error, si no, se envian los datos en la propiedad data
-    $data = [
-        'error' => [
-            'messagge' => 'El usuario no existe'
-        ],
-        'data' => [
-            'id' => "{$result->getId()}",
-            'name' => '{$result->getId()}',
-            'email' => '{$result->getId()}'
-        ]
-    ];
-
-    // devolver siempre el objeto $response
-    return $res->withJson($data, $statusCode);
+    return $res->withJson($result, 200);  
 });
 
 ?>
