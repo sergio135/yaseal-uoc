fc3f99ba67d49c631c5cff9e4e65ec4a2a183973
Sergio Garcia <sergio.garcia@interacso.com>
2018-12-15 18:04:42 +0100

    diseño pagina profile

--- src/routes/profile.php
+++ src/routes/profile.php
@@ -10,16 +10,7 @@ $app->get('/admin_panel/profile', function (Request $req, Response $res, array $
      if (!isset($_SESSION['user'])) {
          return $res->withRedirect('/admin_panel');
      }
-     $adminPanel = new AdminPanelController($this);
-     $news = $adminPanel->listAllNews($req, $res, $args);
-     $nc = new NewsController($this);
-     $categories = $nc->getCategories();
-
-     $args = array("user"=>$_SESSION['user'], "news" => $news, "categories" => $categories);
-     if (isset($_SESSION['notification'])) {
-         $args['notification'] = $_SESSION['notification'];
-         unset($_SESSION['notification']);
-     }
+     
     return $this->view->render($res, 'admin_panel/profile.phtml', $args);
 });
 
